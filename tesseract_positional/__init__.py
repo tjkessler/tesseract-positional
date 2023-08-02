d58dcb98d2075035ba1bbd74ef75e7192845f4f9
tjkessler <travis.j.kessler@gmail.com>
2023-08-02 17:35:13 -0400

    Version pulls from pkg_resources

--- tesseract_positional/__init__.py
+++ tesseract_positional/__init__.py
@@ -1,4 +1,4 @@
 from tesseract_positional.positional_ocr import positional_ocr
 import tesseract_positional.cmd_line
-__version__ = '0.1.1'
-__author__ = 'Travis Kessler <travis.j.kessler@gmail.com>'
+import pkg_resources
+__version__ = pkg_resources.get_distribution('tesseract_positional').version
